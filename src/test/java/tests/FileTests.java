19f307cfb5a3296343e471d5f4e0d27723da7a96
svasenkov <aanher@gmail.com>
2020-06-06 14:50:59 +0300

    Assert local txt files

--- src/test/java/tests/FileTests.java
+++ src/test/java/tests/FileTests.java
@@ -4,8 +4,10 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import utils.FileUtils;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 @Epic("QA.GURU automation course")
@@ -14,11 +16,25 @@ import static org.junit.Assert.assertTrue;
 class FileTests {
 
     @Test
-    void successfulFileCheck() {
+    void successfulSearchForTextInFile() {
+        String expectedFileText = "Here is your text3 in file!";
+        String actualFileText = new FileUtils().readStringFromFile("src/test/resources/files/file.txt");
+        System.out.println("Actual text from file: \n" + actualFileText);
 
-        assertTrue(true);
+        assertThat(actualFileText, containsString(expectedFileText));
 
+//        assertTrue(actualFileText.contains(expectedFileText),
+//                "\nExpected text: \n" + expectedFileText + "\n" +
+//                "Actual text: \n" + actualFileText);
     }
 
+    @Test
+    void successfulSearchForTextInFileRoot() {
+        String expectedFileText = "Here is your text3 in file!";
+        String actualFileText = new FileUtils().readStringFromFile("file_root.txt");
+        System.out.println("Actual text from file: \n" + actualFileText);
+
+        assertThat(actualFileText, containsString(expectedFileText));
+    }
 
 }
