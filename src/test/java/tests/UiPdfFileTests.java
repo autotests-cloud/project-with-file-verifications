e7774c834e32a0f83df82f6ee1b272df065809d3
svasenkov <aanher@gmail.com>
2020-06-06 16:31:09 +0300

    Assert pdf file in jenkins

--- src/test/java/tests/UiPdfFileTests.java
+++ src/test/java/tests/UiPdfFileTests.java
@@ -9,13 +9,13 @@ import org.junit.jupiter.api.Test;
 import utils.FileUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
@@ -23,17 +23,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @Story("File tests")
 @Tag("file")
 @Tag("pdf")
-class PdfFileTests {
+class UiPdfFileTests {
 
     @Test
     void successfulSearchForTextInPdfFile() throws IOException {
+//        Configuration.reportsFolder = <desired location for downloaded files>; чтобы поменять путь скачанных файлов
         String expectedFileText = "pdf_file.pdf";
-        String actualFilePath = "src/test/resources/files/pdf_file.pdf";
 
-        File actualFile = new File(actualFilePath);
+        String jenkinsLogin = "testuser";
+        String jenkinsPassword = "testpassword%";
+
+        open("https://jenkins.autotests.cloud/login");
+        $(byName("j_username")).val(jenkinsLogin);
+        $(byName("j_password")).val(jenkinsPassword).pressEnter();
+        $(withText(jenkinsLogin)).shouldBe(Condition.visible);
+
+        open("https://jenkins.autotests.cloud/job/files_job/ws/src/test/resources/files/");
+
+        File actualFile = $("[href='pdf_file.pdf']").download();
+
         PDF pdf = new PDF(actualFile);
-
         assertThat(pdf, PDF.containsText(expectedFileText));
-            }
+    }
 
 }
